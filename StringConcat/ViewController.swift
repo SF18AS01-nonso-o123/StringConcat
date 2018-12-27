a3b4fa6ce7d51e38e9f60e717d418892a56724c4
Chinonso <chinonsoobidike@chinonsos-mbp.bmcc.cuny.edu>
2018-12-27 11:11:07 -0500

    Initial commit

---
+++ StringConcat/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  StringConcat
+//
+//  Created by Chinonso Obidike on 12/27/18.
+//  Copyright © 2018 Chinonso Obidike. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let label: UILabel = UILabel();
+        label.backgroundColor = .yellow;
+        label.numberOfLines = 0; //0 permits any number of lines
+        
+        label.text = """
+        Close one eye.
+        Stare at the black dot.
+        Do not blink.
+        """;
+        
+        label.frame.origin = CGPoint(x: 100, y: 100); //frame is a CGRect.
+        label.frame.size = label.intrinsicContentSize; //needed if not using contents, intrinsicContentSize also means shrink wrapping, i.e. the cotainer is ec=xactly the size of the cintent
+        print("label.frame = \(label.frame)"); //(100.0, 100.0, 169.0, 61.0)
+        view.addSubview(label);
+    }
+
+
+}
+
---
+++ StringConcatTests/StringConcatTests.swift
@@ -0,0 +1,34 @@
+//
+//  StringConcatTests.swift
+//  StringConcatTests
+//
+//  Created by Chinonso Obidike on 12/27/18.
+//  Copyright © 2018 Chinonso Obidike. All rights reserved.
+//
+
+import XCTest
+@testable import StringConcat
+
+class StringConcatTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
